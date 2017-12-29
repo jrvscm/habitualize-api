95617a2ea707525fe082609fb79efaca24af74fd
Chris Jarvis <jrvscm@gmail.com>
2017-12-28 18:21:21 -0700

    setting up configs

---
+++ server.js
@@ -0,0 +1,92 @@
+'use strict';
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+const uuid = require('uuid');
+
+const {router: usersRouter} = require('./users');
+const {router: authRouter, localStrategy, jwtStrategy} = require('./auth');
+const {CLIENT_ORIGIN} = require('./config');
+
+mongoose.Promise = global.Promise;
+
+const{PORT, DATABASE_URL} = require('./config');
+
+const app = express();
+
+//logging
+app.use(morgan('common'));
+
+//CORS
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	if (req.method === 'OPTIONS') {
+		return res.send(204);
+	}
+	next();
+});
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+app.get('/', (req,res) => {
+	res.json({
+		data: 'everything is a .O.K.'
+	});
+});
+
+app.use('*', (req, res) => {
+	return res.status(404).json({message: 'Not Found'});
+});
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if(err) {
+				return reject(err);
+			}
+			server = app.listen(port,() => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
+}
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if(err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
