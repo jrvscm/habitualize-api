7c161ee3f44f945299ecefb5a7245f29f792abb8
Chris Jarvis <jrvscm@gmail.com>
2018-01-08 22:21:45 -0700

    add express cors middleware

--- config.js
+++ config.js
@@ -3,6 +3,10 @@ exports.DATABASE_URL =
 	process.env.DATABASE_URL ||
 	global.DATABASE_URL ||
 	'mongodb://localhost/habitualize-api';	
+exports.CLIENT_ORIGIN = 
+	process.env.CLIENT_ORIGIN ||
+	global.CLIENT_ORIGIN ||
+	'localhost:3000'
 exports.TEST_DATABASE_URL = 
 	process.env.TEST_DATABASE_URL ||
 	global.TEST_DATABASE_URL ||
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
@@ -10,27 +11,18 @@ const uuid = require('uuid');
 const {router: usersRouter} = require('./users');
 const {router: authRouter, localStrategy, jwtStrategy} = require('./auth');
 const {router: habitRouter} = require('./habits');
-const {CLIENT_ORIGIN} = require('./config');
 
 mongoose.Promise = global.Promise;
 
-const{PORT, DATABASE_URL} = require('./config');
+const{PORT, DATABASE_URL, /*CLIENT_ORIGIN*/} = require('./config');
 
 const app = express();
 
 //logging
 app.use(morgan('common'));
 
-//CORS
-app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-	if (req.method === 'OPTIONS') {
-		return res.send(204);
-	}
-	next();
-});
+//enable client origin before dploying
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
